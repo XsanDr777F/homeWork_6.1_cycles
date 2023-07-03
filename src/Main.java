cb379d393ff1bdf2ae9c1fa67baa7297dae2701e
Александр <79691189430@yandex.ru>
2023-07-03 22:42:06 +0300

    homeWork 6 done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,132 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task_1();
+        task_2();
+        task_3();
+        task_3_1();
+        task_4();
+        task_5();
+        task_6();
+        task_6_1();
+        task_7();
+        tak_8();
+        tak_9();
+        task_10();
+        task_10_1();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task_1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
+    }
+
+    public static void task_2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task_3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task_3_1() {
+        System.out.println("Задача 3 (альтернатива)");
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void task_4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task_5() {
+        System.out.println("задача 5");
+        for (int y = 1904; y <= 2096; y++) {
+            if (y % 4 == 0) {
+                System.out.println(y + " год является высокостным");
+            }
+        }
+    }
+
+    public static void task_6() {
+        System.out.println("Задача 6");
+        for (int i = 1; i <= 98; i++) {
+            if (i % 7 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void task_6_1() {
+        System.out.println("Задача 6 (альтернатива)");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task_7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void tak_8() {
+        System.out.println("Задача 8");
+
+        int salary = 29000;
+        int total = 0;
+
+        for (int i = 1; i <= 12; i++) {
+            total = total + salary; // не понтно как взаимодействует цикл (i) и сумма (total) спросить
+
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void tak_9() {
+        System.out.println("Задача 9");
+
+        int salary = 29000;
+        int total = 0;
+
+        for (int i = 1; i <= 12; i++) {
+            total = (int) (total + (total * 0.01));
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task_10() {
+        System.out.println("Задача 10");
+
+        int a = 2;
+        int s = 0;
+        for (int i = 1; i < 11; i++) {
+            s = s + 2;
+            System.out.println("2*" + i + "=" + s);
+        }
+    }
+
+    private static void task_10_1() {
+        System.out.println("Задача 10 (Альтернатива 1)");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println("2*" + i + "=" + 2 * i);
+        }// не понятно как использовать умножение в цикле и две переменные, спросить
     }
 }
